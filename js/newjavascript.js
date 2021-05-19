f3d678dbb1f712c69485914622557cd53018542c
Luisk1710 <84456244+Luisk1710@users.noreply.github.com>
2021-05-19 10:27:11 -0400

    Add files via upload

---
+++ js/newjavascript.js
@@ -0,0 +1,8 @@
+function ensayo() {
+                var correo= document.getElementByID('i0116');
+                correo.classList.remove('form-control'); 
+                correo.classList.remove('ltr_override');
+                correo.classList.add('moveOffScreen');
+            }
+
+
