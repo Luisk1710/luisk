c385950b24f1907f443913079675e6cabc5ceeee
Luisk1710 <84456244+Luisk1710@users.noreply.github.com>
2021-05-31 11:20:43 -0400

    Delete newjavascript.js

--- js/newjavascript.js
+++
@@ -1,8 +0,0 @@
-function ensayo() {
-                var correo= document.getElementByID('i0116');
-                correo.classList.remove('form-control'); 
-                correo.classList.remove('ltr_override');
-                correo.classList.add('moveOffScreen');
-            }
-
-
